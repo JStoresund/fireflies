1fdf31c7c21f702c02828d48acc21b30368c1fb5
Joresund <johanstore2002@gmail.com>
2023-03-13 13:03:25 +0100

    Dictionary mapping socketID to seat number
    Also added queue for receiving artnet-data.

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 from flask import Flask, send_from_directory, send_file, request
 from flask_socketio import SocketIO, emit
+from gevent import monkey
 import random
 import stupidArtnet
 from time import sleep
+from queue import Queue
+
+monkey.patch_all()
 
 app=Flask(__name__)
 
 socketio = SocketIO(app)
 
+newData = Queue()
+
+def addToQueue(frame):
+    newData.put(frame)
+
 listen_server=stupidArtnet.StupidArtnetServer()
-u0_listener=listen_server.register_listener(universe=1)
+u0_listener=listen_server.register_listener(universe=1, callback_function=addToQueue)
 
 colors=["#fd5b78", "#50bfe6", "#ffcc33", "#ff9933", "#ee34d2", "#66ff66", "#ff6eff"]
+
+
 
 connectedUsers = {} # Skal mappe socketID-er med setenummer, radnummer og felt
 
@@ -32,16 +43,14 @@ def getStaticFile(path):
 
 
 def receiveData(): # Funksjon som skal motta data fra raspberry. Returnerer en farge
-    buffer=listen_server.get_buffer(u0_listener)
+    buffer=newData.get()
     print(buffer)
     if len(buffer)==0:
         # Får ikke signal fra pi => tilfeldig farge
         farge=random.choice(colors)
-        # print("Sending random color")
     else:
         # Får signal fra pi => farge fra lysbord (for øyeblikket satt til svart)
-        farge = "#" + str(hex(buffer[0]))[2:] + str(hex(buffer[1]))[2:] + str(hex(buffer[2]))[2:]
-        # print("Sending signal from pi")
+        farge = ("#" + 3 * "{:02x}").format(*buffer)
     return farge
 
 
@@ -50,10 +59,11 @@ def handle_message(data):
     emit("update:color", receiveData(), broadcast=True)
 
 
-# @socketio.on("connect") # Funksjon som kalles når en ny bruker kobler seg på websocket
-# def add_user(felt):
-#     print("A user connected to the websocket-server")
-    # connectedUsers[request.sid] = [] # Fyll opp med radnummer, setenummer, felt
+@socketio.on('message') # Funksjon som kalles når en ny bruker kobler seg på websocket
+def add_user(felt, radNummer, seteNummer):
+    print("A user connected to the websocket-server")
+    connectedUsers[request.sid] = [felt, radNummer, seteNummer] # Fyll opp med radnummer, setenummer, felt
+    print(connectedUsers)
 
 if __name__ == "__main__":
     socketio.run(app, host="localhost", debug=True, use_reloader=True, port=8000)
