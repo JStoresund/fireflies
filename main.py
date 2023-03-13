a29d3f358f02bd07ed660afe45cf9f4aac7283db
Joresund <johanstore2002@gmail.com>
2023-03-13 15:20:24 +0100

    Added rudimentary wave-function
    Probably needs some more work.
    
    Also made minor changes; connectedUsers now maps a position to a socketID, insted of the other way around. This stops connectedUsers from adding a new element when a user refreshes the site.

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ app=Flask(__name__)
 
 socketio = SocketIO(app)
 
-newData = Queue()
+artnet_data = Queue() # Queue som skal fylles opp med data fra pi fortløpende
 
-def addToQueue(frame):
-    newData.put(frame)
+def addToQueue():
+    artnet_data.put(listen_server.get_buffer())
 
 listen_server=stupidArtnet.StupidArtnetServer()
 u0_listener=listen_server.register_listener(universe=1, callback_function=addToQueue)
@@ -25,6 +25,7 @@ colors=["#fd5b78", "#50bfe6", "#ffcc33", "#ff9933", "#ee34d2", "#66ff66", "#ff6e
 
 
 connectedUsers = {} # Skal mappe socketID-er med setenummer, radnummer og felt
+antallSeter=0
 
 @app.route('/farge')
 def farge():
@@ -40,29 +41,38 @@ def getStaticFile(path):
     return send_from_directory("static", path) 
 
 
-
-
-def receiveData(): # Funksjon som skal motta data fra raspberry. Returnerer en farge
-    buffer=newData.get()
+def receiveData(): # Funksjon som skal motta data fra raspberry. Returnerer en fargeverdi
+    buffer=artnet_data.get()
     print(buffer)
     if len(buffer)==0:
         # Får ikke signal fra pi => tilfeldig farge
-        farge=random.choice(colors)
+        data=random.choice(colors)
     else:
         # Får signal fra pi => farge fra lysbord (for øyeblikket satt til svart)
-        farge = ("#" + 3 * "{:02x}").format(*buffer)
-    return farge
-
+        data = ("#" + 3 * "{:02x}").format(*buffer)
+    return data
 
 @socketio.on('update:color') # Funksjon som kalles når signal kommer fra klient (se websocket.html)
 def handle_message(data):
     emit("update:color", receiveData(), broadcast=True)
 
 
-@socketio.on('message') # Funksjon som kalles når en ny bruker kobler seg på websocket
+def Wave(waveColor, defaultColor, speed):
+    for column in range(1, antallSeter+1):
+        for position, user in connectedUsers:
+            if position[2]==column:
+                emit("update:color", waveColor, room=user)
+            else:
+                emit("update:color", defaultColor, room=user)
+        sleep(500/speed)
+
+
+
+@socketio.on('build:addUser') # Funksjon som kalles når en ny bruker kobler seg på websocket
 def add_user(felt, radNummer, seteNummer):
     print("A user connected to the websocket-server")
-    connectedUsers[request.sid] = [felt, radNummer, seteNummer] # Fyll opp med radnummer, setenummer, felt
+    connectedUsers[ (felt, radNummer, seteNummer) ] = request.sid # Legger bruker inn i dictionary
+    antallSeter=max(antallSeter, seteNummer)
     print(connectedUsers)
 
 if __name__ == "__main__":
